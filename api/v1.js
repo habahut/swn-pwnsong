787ad16b219be828c970dc767f05462a5993aae4
Trevor Haba <habahut@gmail.com>
2015-10-28 22:25:22 -0700

    -added postgres sql library and basic query

--- api/v1.js
+++ api/v1.js
@@ -1,10 +1,26 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    pg = require('pg');
+
+var connectionString = 'postgres://viewer:123@localhost/pwnsong';
+    client = new pg.Client(connectionString);
+client.connect();
 
 app.use("/", express.static(__dirname + "/../frontEnd"));
 
-app.get("/api/v1/planet/:planet", function(req, res) {
-    res.send({"name": "dummy", "description": "dummy2"});
+app.get("/api/v1/planet/:planetName", function(req, res) {
+    var planetName = req.params.planetName,
+        query = client.query("SELECT name, description "
+                           + "FROM Planets "
+                           + "WHERE LOWER(name) = LOWER('" + planetName + "');"),
+        results = [];
+    query.on('row', function(row) {
+        results.push(row);
+    });
+    query.on('end', function() {
+        res.send(results[0]);
+        res.end();
+    });
 });
 
 app.listen(5050);
--- frontEnd/js/planetController.js
+++ frontEnd/js/planetController.js
@@ -6,6 +6,9 @@ app.controller("planetController", function($scope, $http) {
     $scope.init = function() {
         if ($scope.name != "undefined") {
             $scope.ajaxUrl = "/api/v1/planet/" + $scope.name;
+
+        console.log($scope.ajaxUrl);
+
             $http.get($scope.ajaxUrl)
             .success(function(response) {
                 $scope.name = response.name;
