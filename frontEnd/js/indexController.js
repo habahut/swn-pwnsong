457c5e3577c0a09bcd3c0566841b210821b0f771
Trevor Haba <habahut@gmail.com>
2015-11-22 23:43:50 -0800

    added system map to index.html

--- frontEnd/js/indexController.js
+++ frontEnd/js/indexController.js
@@ -5,6 +5,15 @@ app.controller("indexController", function($scope, $http) {
     $scope.init = function() {
         $http.get("/api/v1/systems")
         .success(function(response) {
+            response.forEach(function(system) {
+                var x = Math.floor(system.coords),
+                    // round floating point precision
+                    y = Math.round(system.coords % 1 * 10);
+                system.x = (x - 1) * 95;
+                system.x += (y % 2 != 1) ? 30 : 80;
+                system.y = 325 + (y - 1) * 80 + 48;
+            });
+            console.log(response);
             $scope.systems = response;
         });
     };
